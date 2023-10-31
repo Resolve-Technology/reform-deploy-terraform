f71f40a3ebb98fd60a8174c62341ea5527c0521c
Alan <alan@AlanMacBookAir.local>
2023-10-31 14:55:25 +0800

    Add Vault Terraform for Database with VSO

--- vault/database-vso/resource_kubernetes_secret.tf
+++ vault/database-vso/resource_kubernetes_secret.tf
@@ -1,5 +1,6 @@
 resource "kubernetes_secret" "secret" {
   metadata {
+    name = "default-token"
     namespace = var.k8s_namespace
     annotations = {
       "kubernetes.io/service-account.name" = "default"
