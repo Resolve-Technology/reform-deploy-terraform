ec5d92b58456678fc4a42a98c018404e5b8b0fbf
Alan <alan@AlanMacBookAir.local>
2023-11-02 12:48:58 +0800

    Update VSO creation statements

--- vault/database-vso/resource_vault_database_secret_backend_role.tf
+++ vault/database-vso/resource_vault_database_secret_backend_role.tf
@@ -2,7 +2,7 @@ resource "vault_database_secret_backend_role" "database_secret_backend_role" {
   name                  = var.target_namespace
   backend               = vault_mount.mount.path
   db_name               = vault_database_secret_backend_connection.database_secret_backend_connection.name
-  creation_statements   = ["CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';", "GRANT SELECT ON *.* TO '{{name}}'@'%';"]
+  creation_statements   = ["CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';", "GRANT ALL PRIVILEGES ON ${var.database_name}.* TO '{{name}}'@'%';"]
   revocation_statements = ["DROP USER '{{name}}'@'%';"]
   max_ttl               = var.database_role_max_ttl
 }
