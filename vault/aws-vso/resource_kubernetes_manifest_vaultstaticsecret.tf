f7de06d23ac9f6852abaa311188939eb82299491
Alan <alan@AlanMacBookAir.local>
2023-11-14 00:39:10 +0800

    Update AWS Secret Engine with VSO Integration

--- vault/aws-vso/resource_kubernetes_manifest_vaultstaticsecret.tf
+++ vault/aws-vso/resource_kubernetes_manifest_vaultstaticsecret.tf
@@ -1,41 +1,41 @@
-resource "kubernetes_manifest" "vault_static_secret" {
-  manifest = {
-    apiVersion = "secrets.hashicorp.com/v1beta1"
-    kind       = "VaultStaticSecret"
+# resource "kubernetes_manifest" "vault_static_secret" {
+#   manifest = {
+#     apiVersion = "secrets.hashicorp.com/v1beta1"
+#     kind       = "VaultStaticSecret"
 
-    metadata = {
-      name      = var.context_name
-      namespace = var.target_namespace
-    }
+#     metadata = {
+#       name      = var.context_name
+#       namespace = var.target_namespace
+#     }
 
-    spec = {
-      vaultAuthRef : var.context_name
+#     spec = {
+#       vaultAuthRef : var.context_name
 
-      mount : vault_aws_secret_backend.aws_secret_backend.path
+#       mount : vault_aws_secret_backend.aws_secret_backend.path
 
-      type : "iam_user"
+#       type : "iam_user"
 
-      path : join("/", ["static-creds", vault_aws_secret_backend_static_role.aws_secret_backend_static_role.name])
+#       path : join("/", ["static-creds", vault_aws_secret_backend_static_role.aws_secret_backend_static_role.name])
 
-      destination = {
-        create : true
-        name : join("-", ["vso", var.context_name, "output"])
-      }
+#       destination = {
+#         create : true
+#         name : join("-", ["vso", var.context_name, "output"])
+#       }
 
-      rolloutRestartTargets = [{
-        kind : "Deployment"
-        name : join("-", [var.context_appname, var.target_deployment])
-      }]
-    }
-  }
+#       rolloutRestartTargets = [{
+#         kind : "Deployment"
+#         name : join("-", [var.context_appname, var.target_deployment])
+#       }]
+#     }
+#   }
 
-  wait {
-    fields = {
-      "status.lastRenewalTime" = "^(\\d+){10}$"
-    }
-  }
+#   wait {
+#     fields = {
+#       "status.lastRenewalTime" = "^(\\d+){10}$"
+#     }
+#   }
 
-  depends_on = [
-    kubernetes_manifest.vault_auth
-  ]
-}
+#   depends_on = [
+#     kubernetes_manifest.vault_auth
+#   ]
+# }
