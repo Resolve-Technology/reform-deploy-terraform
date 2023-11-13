2b0ac649fd2641f38a1db2920a3632a845630f24
Alan <alan@AlanMacBookAir.local>
2023-11-13 23:41:32 +0800

    Update AWS Secret Engine with VSO Integration

--- vault/aws-vso/resource_vault_aws_secret_backend.tf
+++ vault/aws-vso/resource_vault_aws_secret_backend.tf
@@ -1,11 +1,11 @@
-resource "vault_aws_secret_backend" "aws_secret_backend" {
-  access_key            = var.vault_root_access_key
-  secret_key            = var.vault_root_secret_key
-  region                = var.bucket_region
-  path                  = join("-", [var.target_namespace, var.bucket_name])
-  max_lease_ttl_seconds = var.max_lease_ttl_seconds
+# resource "vault_aws_secret_backend" "aws_secret_backend" {
+#   access_key            = var.vault_root_access_key
+#   secret_key            = var.vault_root_secret_key
+#   region                = var.bucket_region
+#   path                  = join("-", [var.target_namespace, var.bucket_name])
+#   max_lease_ttl_seconds = var.max_lease_ttl_seconds
 
-  depends_on = [
-    vault_mount.mount
-  ]
-}
+#   depends_on = [
+#     vault_mount.mount
+#   ]
+# }
