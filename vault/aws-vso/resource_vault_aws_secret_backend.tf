4adf9d9675beababe45ca47be088b499e387e108
Alan <alan@AlanMacBookAir.local>
2023-11-13 23:36:28 +0800

    Add AWS Secret Engine with VSO Integration

--- vault/aws-vso/resource_vault_aws_secret_backend.tf
+++ vault/aws-vso/resource_vault_aws_secret_backend.tf
@@ -2,6 +2,6 @@ resource "vault_aws_secret_backend" "aws_secret_backend" {
   access_key            = var.vault_root_access_key
   secret_key            = var.vault_root_secret_key
   region                = var.bucket_region
-  path                  = vault_mount.mount.path
+  path                  = join("-", [var.target_namespace, var.bucket_name])
   max_lease_ttl_seconds = var.max_lease_ttl_seconds
 }
