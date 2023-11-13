957f8f792c6b75abb33319c6324961c41a188ac6
Alan <alan@AlanMacBookAir.local>
2023-11-13 23:20:47 +0800

    Add AWS Secret Engine with VSO Integration

--- vault/aws-vso/resource_vault_policy.tf
+++ vault/aws-vso/resource_vault_policy.tf
@@ -2,15 +2,15 @@ resource "vault_policy" "policy" {
   name = join("-", [var.target_namespace, var.bucket_name])
 
   policy = <<EOT
-path "${join("/", [join("-", [var.target_namespace, var.bucket_name]), "static-creds", vault_database_secret_backend_role.database_secret_backend_role.name])}" {
+path "${join("/", [join("-", [var.target_namespace, var.bucket_name]), "static-creds", vault_aws_secret_backend_static_role.aws_secret_backend_static_role.name])}" {
   capabilities = ["read"]
 }
 
-path "sys/leases/+/${join("/", [join("-", [var.target_namespace, var.bucket_name]), "static-creds", vault_database_secret_backend_role.database_secret_backend_role.name])}/*" {
+path "sys/leases/+/${join("/", [join("-", [var.target_namespace, var.bucket_name]), "static-creds", vault_aws_secret_backend_static_role.aws_secret_backend_static_role.name])}/*" {
   capabilities = [ "create", "read", "update", "delete", "list", "sudo" ]
 }
 
-path "sys/leases/+/${join("/", [join("-", [var.target_namespace, var.bucket_name]), "static-creds", vault_database_secret_backend_role.database_secret_backend_role.name])}" {
+path "sys/leases/+/${join("/", [join("-", [var.target_namespace, var.bucket_name]), "static-creds", vault_aws_secret_backend_static_role.aws_secret_backend_static_role.name])}" {
   capabilities = [ "create", "read", "update", "delete", "list", "sudo" ]
 }
 EOT
